66ce1e508d7852d05063a9352564a605228369ed
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-25 17:35:06 +0300

    add Usermenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,5 +1,8 @@
 export const UserMenu = () => {
     return <div>
+        <use >
+            <svg width="36" height="36"></svg>
+        </use>
         <p>mango@mail.com</p>
         <button>Logout</button>
     </div>
