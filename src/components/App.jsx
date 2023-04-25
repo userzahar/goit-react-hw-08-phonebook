02bc7166c84ddc8bb14dddafaa5c65982220e6d3
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-25 15:42:21 +0300

    add header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
 import { Form } from "./Form/Form";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
+import { Route, Routes,NavLink } from "react-router-dom";
+import { Header, NavigationList } from "./App.styled";
+
 
 export function App() {
   return <>
+    <Header>
+      <nav>
+        <NavigationList>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/contacts">Contacts</NavLink></li>
+          <li><NavLink to="/register">Registration</NavLink></li>
+          <li><NavLink to="/login">Login</NavLink></li>
+        </NavigationList>
+      </nav>
+    </Header>
+    <main>
       <h1>Phonebook</h1>
       <Form />
       <h2>Contacts</h2>
     <Filter />
-    <ContactList    />
+      <ContactList />
+    </main>
+    <footer></footer>
   </>
 }
  
---
+++ src/components/App.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  background-color: rgb(213, 208, 204);
+  height: 40px;
+  padding-top: 8px;
+`;
+
+export const NavigationList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 0;
+  padding: 0;
+`;
