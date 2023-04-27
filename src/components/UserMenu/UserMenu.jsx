21dc16ae0a79ccaa256e26f83df630048ce3d12a
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-27 15:15:33 +0300

    add usermenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -3,14 +3,14 @@ import { UserMenuStyled } from "./UserMenu.styled"
 import { authOperations } from "redux/auth/thunkAuth";
 const DEFAULT_AVATAR = "https://alternative.me/media/256/wintoflash-icon-scrskb6824tdqyzn-c.png";
 export const UserMenu = () => {
-    const username = useSelector(state => state.auth.user.name);
+    const email = useSelector(state => state.auth.user.email);
     const dispatch = useDispatch();
     const heandleLogout = () => {
         dispatch(authOperations.logOut())
     }
     return <UserMenuStyled>
   <img src={DEFAULT_AVATAR} alt="Аватарка" />
-        <p>Ласкаво просимо {username}!</p>
+        <p>{email}!</p>
   <button onClick={heandleLogout}>Logout</button>
 </UserMenuStyled>
 }
