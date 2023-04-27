4cec9ab9b3bfaaf43ed3b60a65f816b40e00d9ad
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-27 14:37:13 +0300

    add go to

--- src/components/Greetings/Greetings.jsx
+++ src/components/Greetings/Greetings.jsx
@@ -1,7 +1,10 @@
 import { useSelector } from "react-redux";
 import { GreetingsStyled } from "./Greetings.styled";
 
+import { LinkStyled } from "components/ModalLogIn/ModalLogin.styled";
+
 export const Greetings = () => {
 const username = useSelector(state => state.auth.user.name);
-    return <GreetingsStyled>Greetings {username}!</GreetingsStyled>
+    return <><GreetingsStyled>Greetings {username}!</GreetingsStyled>
+   <p>Go to <LinkStyled to="/contacts">Contacts</LinkStyled></p></>
 }
